45b43937c831380ca8d20237ec9f5fd664b5f36e
DigbijayineeClara07 <digbijayinee.clara@stackguardian.io>
2025-09-25 18:44:52 +0530

    added-ansible

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  type = string
+  default = "eu-central-1"
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
